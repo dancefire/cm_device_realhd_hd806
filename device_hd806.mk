1e15f5a378090ace28773cf873c196209cd58a44
Tao Wang <dancefire@gmail.com>
2012-05-11 08:33:57 +1000

    Modified for HD806. The CWM Recovery is working now.
    Modified BoardConfig.mk for HD806 specs.
    Add prebuilt folder, and put kernel mods, initlogos, remotecfg, set_display_mode.sh in it.
    Modified recovery.fstab for HD806.
    Add custom recovery key files: recovery/recovery_ui.c
    Add ueventd.hd806.rc, for device-specified ueventd configuration.
    Add vendorsetup.sh to show up in 'lunch' menu.

--- device_hd806.mk
+++ device_hd806.mk
@@ -17,8 +17,35 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+PRODUCT_COPY_FILES += \
+    device/realhd/hd806/ueventd.hd806.rc:root/ueventd.hd806.rc \
+    device/realhd/hd806/prebuilt/initlogo.480p.rle:root/initlogo.480p.rle \
+    device/realhd/hd806/prebuilt/initlogo.720p.rle:root/initlogo.720p.rle \
+    device/realhd/hd806/prebuilt/initlogo.1080p.rle:root/initlogo.1080p.rle
+
+# Kernel Modules
+PRODUCT_COPY_FILES += \
+    device/realhd/hd806/prebuilt/boot/mali.ko:root/boot/mali.ko \
+    device/realhd/hd806/prebuilt/boot/ump.ko:root/boot/ump.ko
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    device/realhd/hd806/prebuilt/remotecfg:recovery/root/sbin/remotecfg \
+    device/realhd/hd806/prebuilt/set_display_mode.sh:recovery/root/sbin/set_display_mode.sh \
+    device/realhd/hd806/ueventd.hd806.rc:recovery/root/ueventd.hd806.rc \
+    device/realhd/hd806/prebuilt/initlogo.480p.rle:recovery/root/initlogo.480p.rle \
+    device/realhd/hd806/prebuilt/initlogo.720p.rle:recovery/root/initlogo.720p.rle \
+    device/realhd/hd806/prebuilt/initlogo.1080p.rle:recovery/root/initlogo.1080p.rle
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_hd806
 PRODUCT_DEVICE := hd806
+PRODUCT_MANUFACTURER := RealHD
+PRODUCT_BRAND := HDVision
+PRODUCT_MODEL := HD806
+
+PRODUCT_DEFAULT_LANGUAGE := zh
+PRODUCT_DEFAULT_REGION := CN
+PRODUCT_DEFAULT_WIFI_CHANNELS := 14
